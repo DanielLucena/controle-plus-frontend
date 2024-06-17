73920aa081f0604c4578faad355a8d6bfb1f9336
GabrielViniciusLCM <gabrielviniciuslcm@gmail.com>
2024-06-17 18:47:34 -0300

    feat: homepages personalizadas adicionadas.

--- src/components/AppContent.tsx
+++ src/components/AppContent.tsx
@@ -5,69 +5,73 @@ import Buttons from './Buttons';
 import AuthContent from './AuthContent';
 import LoginForm from './LoginForm';
 import WelcomeContent from './WelcomeContent';
+import LoggedInWelcomeContent from './LoggedInWelcomeContent';
 import FuncionarioPage from '../pages/FuncionarioPage';
 import NavBar from './NavBar';
 import FornecedorPage from '../pages/FornecedorPage';
 import ClientePage from '../pages/ClientePage';
 
 const AppContent: React.FC = () => {
-  const [componentToShow, setComponentToShow] = useState<string>('welcome');
-  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+    const [componentToShow, setComponentToShow] = useState<string>('welcome');
+    const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+    const [username, setUsername] = useState<string>('');
 
-  const login = () => {
-    setComponentToShow('login');
-  };
+    const login = () => {
+        setComponentToShow('login');
+    };
 
-  const logout = () => {
-    setComponentToShow('welcome');
-    setAuthHeader(null);
-    setIsLoggedIn(false);
-  };
+    const logout = () => {
+        setComponentToShow('welcome');
+        setAuthHeader(null);
+        setIsLoggedIn(false);
+        setUsername('');
+    };
 
-  const onLogin = async (e: React.FormEvent<HTMLFormElement>, username: string, senha: string) => {
-    e.preventDefault();
-    try {
-      const response = await request('POST', '/usuario/auth', {
-        login: username,
-        senha: senha,
-      });
-      setAuthHeader(response.data.token);
-      setIsLoggedIn(true);
-      setComponentToShow('messages');
-    } catch (error) {
-      setAuthHeader(null);
-      setComponentToShow('welcome');
-    }
-  };
+    const onLogin = async (e: React.FormEvent<HTMLFormElement>, username: string, senha: string) => {
+        e.preventDefault();
+        try {
+            const response = await request('POST', '/usuario/auth', {
+                login: username,
+                senha: senha,
+            });
+            setAuthHeader(response.data.token);
+            setIsLoggedIn(true);
+            setUsername(username);
+            setComponentToShow('loggedInWelcome');
+        } catch (error) {
+            setAuthHeader(null);
+            setComponentToShow('welcome');
+        }
+    };
 
-  const onRegister = async (e: React.FormEvent<HTMLFormElement>, username: string, senha: string, role: string) => {
-    e.preventDefault();
-    try {
-      const response = await request('POST', '/usuario', {
-        login: username,
-        senha: senha,
-        role: role,
-      });
-      onLogin(e, response.data.login, senha);
-    } catch (error) {
-      setAuthHeader(null);
-      setComponentToShow('welcome');
-    }
-  };
+    const onRegister = async (e: React.FormEvent<HTMLFormElement>, username: string, senha: string, role: string) => {
+        e.preventDefault();
+        try {
+            const response = await request('POST', '/usuario', {
+                login: username,
+                senha: senha,
+                role: role,
+            });
+            onLogin(e, response.data.login, senha);
+        } catch (error) {
+            setAuthHeader(null);
+            setComponentToShow('welcome');
+        }
+    };
 
-  return (
-    <>
-      {isLoggedIn && <NavBar onNavigate={setComponentToShow} />}
-      <Buttons login={login} logout={logout} />
-
-      {componentToShow === 'welcome' && <WelcomeContent />}
-      {componentToShow === 'login' && <LoginForm onLogin={onLogin} onRegister={onRegister} />}
-      {componentToShow === 'messages' && <AuthContent />}
-      {componentToShow === 'funcionarios' && <FuncionarioPage />}
-      {componentToShow === 'fornecedores' && <FornecedorPage />}
-      {componentToShow === 'clientes' && <ClientePage />}
-    </>
-  );
+    return (
+        <>
+            {isLoggedIn && <NavBar onNavigate={setComponentToShow} isLoggedIn={isLoggedIn} />}
+            <Buttons login={login} logout={logout} isLoggedIn={isLoggedIn} />
+            {!isLoggedIn && componentToShow === 'welcome' && <WelcomeContent />}
+            {isLoggedIn && componentToShow === 'loggedInWelcome' && <LoggedInWelcomeContent username={username} />}
+            {componentToShow === 'login' && <LoginForm onLogin={onLogin} onRegister={onRegister} />}
+            {componentToShow === 'messages' && <AuthContent />}
+            {componentToShow === 'funcionarios' && <FuncionarioPage />}
+            {componentToShow === 'fornecedores' && <FornecedorPage />}
+            {componentToShow === 'clientes' && <ClientePage />}
+        </>
+    );
 };
 
 export default AppContent;
--- src/components/WelcomeContent.tsx
+++ src/components/WelcomeContent.tsx
@@ -5,8 +5,8 @@ function WelcomeContent() {
         <div className="row justify-content-md-center">
             <div className="jumbotron jumbotron-fluid">
               <div className="container">
-                <h1 className="display-4">Welcome</h1>
-                <p className="lead">Login to see protected content.</p>
+                <h1 className="display-4">Boas vindas!</h1>
+                <p className="lead">Faça o login para ter acesso.</p>
               </div>
             </div>
         </div>
