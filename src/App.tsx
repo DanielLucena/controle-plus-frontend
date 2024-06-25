49c123985eb3477eb6ca0e3421eb97af712ad181
GabrielViniciusLCM <gabrielviniciuslcm@gmail.com>
2024-06-25 19:24:27 -0300

    feat: retirada do "frontend autenticated with JWT

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,6 @@ import AppContent from './components/AppContent';
 function App() {
   return (
     <div className="App">
-      <Header pageTitle="Frontend authenticated with JWT" logoSrc={logo} />
       <div className="container-fluid">
         <div className="row">
           <div className="col">
